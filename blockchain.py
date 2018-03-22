7e06abbc068c595fa7d697d78c3ab978dbb5c439
zgljl2012 <2693491512@qq.com>
2018-03-22 14:46:34 +0800

    blockchain.py

---
+++ blockchain.py
@@ -0,0 +1,98 @@
+"""
+
+区块链定义
+
+@author zgljl2012
+@date   2018/03/21
+"""
+
+from time import time
+import json
+import hashlib
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+        
+    def new_block(self, proof, previous_hash):
+        """
+        生成新区块
+        :param proof: <int> 工作量证明
+        :param previous_hash: (Optional) <str> 前一个区块的Hash值
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+    
+    def new_transaction(self, sender, recipient, amount):
+        """
+        新交易信息，此信息将加入到下一个待挖的区块中
+        :param sender: <str> 发送者
+        :param recipient: <str> 接收者
+        :param amount: <int> 数额
+        :return: <int> 区块索引
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+    
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        生成块的 SHA-256 hash值
+        :param block: <dict> 区块
+        :return: <str> hash值
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        """
+        工作量证明
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        工作量验证
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:2] == "00"
